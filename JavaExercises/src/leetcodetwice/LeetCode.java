31675fb5db4fd36e25d99a1fb875905f54e8f757
ericsonyc <1220246394@qq.com>
2015-11-04 21:19:20 +0800

    update leetcode

--- JavaExercises/src/leetcodetwice/LeetCode.java
+++ JavaExercises/src/leetcodetwice/LeetCode.java
@@ -2795,11 +2795,34 @@ public class LeetCode {
         return little.next;
     }
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        if (j >= 0) {
+            while (j >= 0) {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
     public static void main(String[] args) {
         LeetCode leetCode = new LeetCode();
 
-        char[][] matrix = {{'1', '0', '1', '0'}, {'1', '0', '1', '1'}, {'1', '0', '1', '1'}, {'1', '1', '1', '1'}};
-        leetCode.maximalRectangle2(matrix);
+        int[] nums1 = new int[1];
+        int[] nums2 = new int[1];
+        nums2[0] = 1;
+        leetCode.merge(nums1, 0, nums2, 1);
+
+//        char[][] matrix = {{'1', '0', '1', '0'}, {'1', '0', '1', '1'}, {'1', '0', '1', '1'}, {'1', '1', '1', '1'}};
+//        leetCode.maximalRectangle2(matrix);
 
 //        int[] nums = {1};
 //        System.out.println(leetCode.largestRectangleArea(nums));
