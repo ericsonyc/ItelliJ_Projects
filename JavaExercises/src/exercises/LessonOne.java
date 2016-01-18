6beba2d3e900472b004141f317c771cfc5d57c57
ericsonyc <1220246394@qq.com>
2016-01-18 14:10:04 +0800

    update

---
+++ JavaExercises/src/exercises/LessonOne.java
@@ -0,0 +1,15 @@
+package exercises;
+
+/**
+ * Created by ericson on 2016/1/12 0012.
+ */
+public class LessonOne {
+
+    protected void getVertex(){
+        System.out.println("LessonOne");
+    }
+
+    public static void main(String[] args){
+
+    }
+}
